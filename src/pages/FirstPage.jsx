ea7cf68d8ff10151bfa7f15bbe5566f4d982c1f3
solux <lee.yeji0327@gmail.com>
2023-11-07 00:35:39 +0900

    [add] firstpage 틀 구현

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import logo_2023 from '../images/logo_2023.svg';
+import logo from '../images/logo.svg';
+import bell from '../images/3dicon/bell.jpg';
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    overflow-x: hidden;
+    background-color: #262626;;
+    align-items: center;
+    text-align: center;
+`
+const LogoBlock = styled.div`
+    padding-top: 5vh;
+`
+const Logo = styled.div`
+    margin-top: -15px;
+`
 
 const FirstPage = () => {
     return (
-        <div>
-            
-        </div>
+        <Wrapper>
+            <LogoBlock>
+                <img src={logo_2023}/>
+                <Logo><img src={logo}/></Logo>
+            </LogoBlock>
+        </Wrapper>
     );
 };
 
