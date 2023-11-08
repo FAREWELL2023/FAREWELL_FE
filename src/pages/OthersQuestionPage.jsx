9f7084c107565719b8026101b07d13428f8de48c
Ivoryeee <psa109@sookmyung.ac.kr>
2023-11-08 12:25:34 +0900

    [feat] OthersQuestionPage 구현

--- src/pages/OthersQuestionPage.jsx
+++ src/pages/OthersQuestionPage.jsx
@@ -11,7 +11,7 @@ const Wrapper = styled.div`
     overflow-x: hidden;
     background-color: #262626;;
     align-items: center;
-    text-align: center;
+    //text-align: center;
 `;
 
 const Logo = styled.div`
@@ -20,30 +20,50 @@ const Logo = styled.div`
     right: 30px;
 `
 const Title = styled.div`
-    color: #FFF;
+    color: #EFEC69;
     font-weight: 700;
-    margin: 1vh 0 3vh 0;
-    font-size: 1.2rem;
+    margin: 0 0 1vh 10vw;
+    font-size: 1.5rem;
 `
 const TitleTxt1=styled.div`
     color: #FFF;
-    font-weight: 500;
-    margin: 1vh 0 3vh 0;
-    font-size: 1.0rem;
+    font-weight: 400;
+    margin: 3vh 0 0 11vw;
+    font-size: 0.9rem;
 `;
 
 const TitleTxt2=styled.div`
     color: #FFF;
-    font-weight: 700;
-    margin: 1vh 0 3vh 0;
-    font-size: 0.8rem;
+    font-weight: 500;
+    margin: 1vh 0 3vh 11vw;
+    font-size: 0.6rem;
 `;
 
 const QuestionList=styled.div`
     background-color:#FFF;
+    color:#262626;
+    width:81vw;
+    height:5.5vh;
+    line-height:5.5vh;
+    text-align:center;
+    font-size: 0.8rem;
+    margin: 1.5vh auto;
+    border-radius:41.85px;
 `;
 
+const questionlist = [
+    {number: 1, question: "Q. 올해 이 사람에게 가장 고마웠던 일은?"},
+    {number: 2, question: "Q. 올해 이 사람에게 가장 미안했던 일은?"},
+    {number: 3, question: "Q. 올해 이 사람이 가장 빛났던 순간은?"},
+    {number: 4, question: "Q. 올해 이 사람이 가장 웃겼던 순간은?"},
+    {number: 5, question: "Q. 올해 이 사람과 함께한 행복했던 순간은?"},
+    {number: 6, question: "Q. 올해 이 사람과 함께한 잊을 수 없던 순간"},
+    {number: 7, question: "Q. 올해 이 사람에게 하고싶었지만 못 했던 말"},
+    {number: 8, question: "Q. 2023을 마무리하며 이 사람에게 한 마디"}
+];
+
 const OthersQuestionPage = () => {
+
     return (
         <Wrapper>    
             <Logo><img src={tree} style={{width: "22vw", height:"17vh"}}/></Logo>
@@ -51,9 +71,11 @@ const OthersQuestionPage = () => {
             <TitleTxt1>name님에게 한마디</TitleTxt1>
             <Title>질문 선택하기</Title>
             <TitleTxt2>질문에 답변하여 name님의 회고록을 채워주세요.</TitleTxt2>
-            <QuestionList>
-                Q.~~~~
-            </QuestionList>
+            {questionlist.map((item) => (
+                <QuestionList key={item.number}>
+                    {item.question}
+                </QuestionList>
+            ))}
         </Wrapper>
     );
 };
